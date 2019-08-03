092ee37722fc3f5960b9ef6fc5390df10e1340d5
mishagodovanuk <mishagodovanuk@gmail.com>
2019-08-03 16:32:25 +0300

    added get product sku

--- Catalog/Block/Catalog/RequestPriceForm.php
+++ Catalog/Block/Catalog/RequestPriceForm.php
@@ -9,6 +9,8 @@
 namespace Smile\Catalog\Block\Catalog;
 
 use Magento\Framework\View\Element\Template;
+use Magento\Framework\Registry;
+use Magento\Backend\Block\Template\Context;
 
 /**
  * Class RequestPriceForm
@@ -17,6 +19,27 @@ use Magento\Framework\View\Element\Template;
 class RequestPriceForm extends Template
 {
     /**
+     * @var Registry
+     */
+    protected $_registry;
+
+    /**
+     * RequestPriceForm constructor.
+     * @param Context $context
+     * @param Registry $registry
+     * @param array $data
+     */
+    public function __construct(
+        Context $context,
+        Registry $registry,
+        array $data = []
+    )
+    {
+        $this->_registry = $registry;
+        parent::__construct($context, $data);
+    }
+
+    /**
      * Get form action
      *
      * @return string
@@ -24,5 +47,25 @@ class RequestPriceForm extends Template
     public function getFormAction()
     {
         return $this->getUrl('smile_customer/request/save');
+    }
+
+    /**
+     * Get current category
+     *
+     * @return object
+     */
+    public function getCurrentCategory()
+    {
+        return $this->_registry->registry('current_category');
+    }
+
+    /**
+     * Get current product
+     *
+     * @return object
+     */
+    public function getCurrentProduct()
+    {
+        return $this->_registry->registry('current_product');
     }
 }
