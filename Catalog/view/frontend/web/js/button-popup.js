3cda84843cd04cfaee6fc42f896b4b6985d7df92
mishagodovanuk <mishagodovanuk@gmail.com>
2019-08-07 22:38:39 +0300

    add send button

--- Catalog/view/frontend/web/js/button-popup.js
+++ Catalog/view/frontend/web/js/button-popup.js
@@ -10,6 +10,7 @@ define(
         $,
         modal
     ) {
+        var $form = $('#request-price-form');
         var options = {
             type: 'popup',
             responsive: true,
@@ -21,12 +22,25 @@ define(
                 click: function () {
                     this.closeModal();
                 }
-            }]
+            },
+                {
+                    text: $.mage.__('Send'),
+                    class: 'action submit primary',
+                    click: function () {
+                        sendFormData()
+                    }
+                }
+            ]
         };
 
         var popup = modal(options, $('#popup-modal-form'));
         $("#request-price-button").on('click',function(){
             $("#popup-modal-form").modal("openModal");
         });
+
+        function sendFormData() {
+            alert('send form data');
+            //Ajax logic
+        }
     }
 );
