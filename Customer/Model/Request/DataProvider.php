40b4df1e62d4559348f804a177928e0f928fdfb7
mishagodovanuk <mishagodovanuk@gmail.com>
2019-08-10 20:58:39 +0300

    email smtp

--- Customer/Model/Request/DataProvider.php
+++ Customer/Model/Request/DataProvider.php
@@ -42,9 +42,9 @@ class DataProvider extends AbstractDataProvider
     /**
      * DataProvider constructor.
      *
-     * @param $name
-     * @param $primaryFieldName
-     * @param $requestFieldName
+     * @param string $name
+     * @param string $primaryFieldName
+     * @param string $requestFieldName
      * @param CollectionFactory $requestCollectionFactory
      * @param DataPersistorInterface $dataPersistor
      * @param StoreManagerInterface $storeManager
